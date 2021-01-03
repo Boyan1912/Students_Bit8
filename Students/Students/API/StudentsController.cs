47543f63991f383630a8d70b75b225ed8d9169e4
Boyan1912 <Boyan.Denkov@bulpros.com>
2021-01-03 17:18:34 +0200

    Students API infrastructure added

---
+++ Students/Students/API/StudentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Students.Models;
+using Students.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Students.API
+{
+    [ApiController]
+    public class StudentsController
+    {
+        private readonly IConfiguration _config;
+        private readonly IStudentsService _service;
+
+        public StudentsController(IConfiguration config, IStudentsService service)
+        {
+            _config = config;
+            _service = service;
+        }
+
+        [HttpGet("Students/GetData")]
+        public async Task<IActionResult> GetData()
+        {
+            var result = new ApiResultModel<Student>();
+
+            try
+            {
+                var semesters = await _service.GetAll();
+                result.Data = semesters;
+            }
+            catch (Exception ex)
+            {
+                result.ErrorMessage = ex.Message;
+            }
+
+            return new JsonResult(result);
+        }
+    }
+}
