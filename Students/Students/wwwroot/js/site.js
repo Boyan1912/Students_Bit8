95a44afdc3066a9388fa64615492c14307d1291e
Boyan1912 <Boyan.Denkov@bulpros.com>
2021-01-03 13:38:06 +0200

    js code cleaned and rearranged

--- Students/Students/wwwroot/js/site.js
+++ Students/Students/wwwroot/js/site.js
@@ -27,3 +27,40 @@
         })
     }
 }
+
+function editDiscipline(discipline, btn) {
+    var editMode = btn.html() === 'Edit'
+    var input = btn.prev('input');
+    if (editMode) {
+        var oldVal = $(input).val();
+        btn.html('Done');
+        $(input).removeAttr('readonly')
+                    .attr('value', '')
+                    .focus();
+    }
+    else {
+        $.ajax({
+            url: "/Disciplines/Edit?id=" + discipline.IdDiscipline + "&professor=" + $(input).val(),
+            method: "PUT",
+            contentType: 'application/json'
+        })
+        .done(function( res ) {
+            if (!!res.ErrorMessage) {
+                alert(res.ErrorMessage);
+                $(input).val(oldVal);
+            }
+            else {
+                            
+            }
+            })
+        .fail(function(err) {
+            console.error(err.responseText);
+            alert( "Error editing discipline!");
+            $(input).val(oldVal);
+            })
+        .always(function() {
+            btn.html('Edit');
+            $(input).attr('readonly', 'readonly');
+        })
+    }
+}
