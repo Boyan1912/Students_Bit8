ede7322ee0bfcc9e49227d87f879366f2c6eda34
Boyan1912 <Boyan.Denkov@bulpros.com>
2021-01-03 12:58:28 +0200

    delete discipline from the Semesters view implemented with FE validation

--- Students/Students/wwwroot/js/site.js
+++ Students/Students/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function deleteDiscipline(discipline, row) {
+    debugger;
+    if (!!discipline.Score) {
+        alert("Only disciplines without scores can be deleted!")
+    }
+    else {
+        $.ajax({
+            url: "/Disciplines/Delete?id=" + discipline.IdDiscipline,
+            method: "DELETE",
+            contentType: 'application/json'
+        })
+        .done(function( res ) {
+            if (!!res.ErrorMessage) {
+                alert(res.ErrorMessage);
+            }
+            else {
+                if (!!row) {
+                    row.remove().draw();
+                }
+                else {
+                    location.reload(true);
+                }
+            }
+        })
+        .fail(function(err) {
+            console.error(err.responseText);
+            alert( "Error deleting discipline!");
+        })
+    }
+}
