5d6f831090e2bafb7da327393e9157055c50a542
Boyan1912 <Boyan.Denkov@bulpros.com>
2021-01-06 01:24:05 +0200

    database changes and new seed data

--- alter_schema.sql
+++ alter_schema.sql
@@ -18,3 +18,28 @@ ADD CONSTRAINT `fk_discipline_semester`
   FOREIGN KEY (`id_semester`)
   REFERENCES `mydb`.`semester` (`id_semester`)
   ON DELETE CASCADE;
+  
+  
+ALTER TABLE `mydb`.`semester` 
+DROP FOREIGN KEY `fk_semester_student`;
+ALTER TABLE `mydb`.`semester` 
+DROP COLUMN `id_student`,
+ADD COLUMN `start_date` DATETIME NULL AFTER `name`,
+ADD COLUMN `end_date` DATETIME NULL AFTER `start_date`,
+CHANGE COLUMN `name` `name` VARCHAR(100) NOT NULL ,
+DROP INDEX `fk_semester_student_idx` ;
+;
+
+ALTER TABLE `mydb`.`student` 
+DROP FOREIGN KEY `fk_student_semester`;
+ALTER TABLE `mydb`.`student` 
+DROP COLUMN `id_semester`,
+ADD COLUMN `date_of_birth` DATETIME NULL AFTER `last_name`,
+DROP INDEX `fk_student_semester_idx` ;
+;
+
+CREATE TABLE `mydb`.`students_semesters` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `id_student` INT NULL,
+  `id_semester` INT NULL,
+  PRIMARY KEY (`id`));
